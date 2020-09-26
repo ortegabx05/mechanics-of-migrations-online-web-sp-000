c85e6a54c6cd4f84a1c0aedb654d397571058718
jessi ortega <ortegabx05@gmail.com>
2020-09-26 15:28:55 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,12 +1,6 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
- 
-  def down
-  end
-end
 
-  def change
+    def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
